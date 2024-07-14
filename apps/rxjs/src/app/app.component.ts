1810e77e75537e3e8e2101e261775044b94b6284
Madi-Naf <onplayer6@gmail.com>
2024-07-14 18:37:31 +0200

    rxjs

--- apps/rxjs/src/app/app.component.ts
+++ apps/rxjs/src/app/app.component.ts
@@ -6,15 +6,20 @@ import { Ex4Component } from './ex4/ex4.component';
 import { Ex5Component } from './ex5/ex5.component';
 
 @Component({
+  selector: 'app-root',
   standalone: true,
   imports: [Ex1Component, Ex2Component, Ex3Component, Ex4Component, Ex5Component],
-  selector: 'app-root',
   template: `
-    <app-ex1/>
-    <!-- <app-ex2/> -->
-    <!-- <app-ex3/> -->
-    <!-- <app-ex4/> -->
-    <!-- <app-ex5/> -->
+    <h1>
+      Rxjs Exercice
+    </h1>
+
+    <app-ex1 />
+    <app-ex2 />
+    <app-ex3 /> 
+    <app-ex4 />
+    <!-- <app-ex5 /> -->
   `
 })
-export class AppComponent {}
+export class AppComponent {
+}
--- apps/rxjs/src/app/ex4/ex4.component.ts
+++ apps/rxjs/src/app/ex4/ex4.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter, interval, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-ex4',
   standalone: true,
   imports: [],
-  template: ''
+  template: `
+    <h2> Exercice 4 </h2>
+    <div style="display: flex;">
+      @for (r of result; track r) {
+        <p>{{ r }} </p>
+      }
+    </div>
+  `
 })
-export class Ex4Component {
+export class Ex4Component implements OnInit {
   // TODO : Utiliser un interval pour émettre des valeurs
-  // TODO : Utiliser un opérateur pour limiter le nombre de valeurs émises à 10
-  // TODO : Utiliser un opérateur pour limiter les valeurs émises en fonction d'une condition
+  _count: number = 0
+  private _values$ = interval(1000).pipe(map(_ => {
+    this._count += 1
+    return this._count
+  }))
+
   // TODO : Utiliser un opérateur pour émettre les 5 dernières valeurs
   
+
+  result: Array<number> = []
+
   constructor() {}
+  ngOnInit(): void {
+    console.log('---------------- exercice 3 ----------------')
+    this._values$
+    .pipe(
+      // TODO : Utiliser un opérateur pour limiter le nombre de valeurs émises à 10
+      take(10),
+      // TODO : Utiliser un opérateur pour limiter les valeurs émises en fonction d'une condition
+      filter(value => value % 2 === 0))
+    .subscribe(value => {
+      console.log(value)
+      this.result.push(value)
+    })
+  }
 }

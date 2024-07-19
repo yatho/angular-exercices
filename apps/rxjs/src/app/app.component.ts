fd3e01a3207867bcbfa6f710d2a255ec4f8f52b2
Yann-Thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2024-07-19 15:12:49 +0200

    Ajout d'un composant de manipulation de subject pour essayer de comprendre les différences

--- apps/rxjs/src/app/app.component.ts
+++ apps/rxjs/src/app/app.component.ts
@@ -4,17 +4,26 @@ import { Ex2Component } from './ex2/ex2.component';
 import { Ex3Component } from './ex3/ex3.component';
 import { Ex4Component } from './ex4/ex4.component';
 import { Ex5Component } from './ex5/ex5.component';
+import { SubjectComponent } from './subject/subject.component';
 
 @Component({
   standalone: true,
-  imports: [Ex1Component, Ex2Component, Ex3Component, Ex4Component, Ex5Component],
+  imports: [
+    Ex1Component,
+    Ex2Component,
+    Ex3Component,
+    Ex4Component,
+    Ex5Component,
+    SubjectComponent,
+  ],
   selector: 'app-root',
   template: `
-    <app-ex1/>
+    <!-- <app-ex1/> -->
     <!-- <app-ex2/> -->
     <!-- <app-ex3/> -->
     <!-- <app-ex4/> -->
     <!-- <app-ex5/> -->
-  `
+    <app-subject />
+  `,
 })
 export class AppComponent {}
